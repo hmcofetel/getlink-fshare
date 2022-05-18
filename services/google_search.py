201ea6505b874b8ad77f26b060e22845bc21074e
hmcofetel <minhco1232001@gmail.com>
2022-05-18 23:43:28 +0700

    Fix

--- services/google_search.py
+++ services/google_search.py
@@ -15,7 +15,3 @@ class GoogleSearch:
 		try:
 			return [item['link'] for item in data['items'] ]
 		except: return []
-
-if __name__ == '__main__':
-	app = GoogleSearch('AIzaSyC_4MhhBWREyn7S7qpQN348rRb6HH_Um_8','c7954a67903197864')
-	search = app.get_items_search('conan')
